a5dce953f66c964726aea34de1083713bc3f8315
aleriiu <aleriiiu@gmail.com>
2025-01-31 16:44:18 +0300

    small changes

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -5,7 +5,7 @@ export default function TextField() {
     return (
         <div className='text-field-container'>
             <label className='text-field-label'><Translation path="text-field-label" />&nbsp;(&nbsp;<span className='text-field-label-span'><Translation path="text-field-label-span" /></span>&nbsp;)</label>
-            <textarea rows={3} 
+            <textarea rows={4} 
                 placeholder="введите описание/Enter your description"
             />
            
